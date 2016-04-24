405a296314f52ee8a47b8be2933d2e8d23ebbcfc
Priyatham Kattakinda <kmspriyatham@gmail.com>
2016-04-25 04:27:26 +0530

    optimised solve

--- PoissonSolver.cpp
+++ PoissonSolver.cpp
@@ -10,6 +10,9 @@
 #include <Eigen/Sparse>
 #include <Eigen/UmfPackSupport>
 #include <vector>
+//#include <iostream>
+
+Eigen::UmfPackLU<Eigen::SparseMatrix<double>> solver;
 
 unsigned int PoissonSolver::numNeighbors(Point p) {
     unsigned int numNeighbors = 4;
@@ -60,16 +63,13 @@ void PoissonSolver::compute() {
         }
     }
     A.setFromTriplets(coefficients.begin(), coefficients.end());
-    Eigen::UmfPackLU<Eigen::SparseMatrix<double>> solver;
+//    Eigen::UmfPackLU<Eigen::SparseMatrix<double>> solver;
     solver.compute(A);
+    //std::cout << "computed" << std::endl;
 }
 
 Eigen::MatrixXd PoissonSolver::solve(std::function<double(Point)> dirichlet, std::function<double(Point, Point)> guidance) {
     unsigned int n = (unsigned int) domain.size();
-    Eigen::SparseMatrix<double, Eigen::ColMajor> A(n, n);
-    A.reserve(4 * n);
-    std::vector<Eigen::Triplet<double>> coefficients;
-    coefficients.reserve(4 * n);
     Eigen::MatrixXd b(n, 1), x(n, 1);
     b.setZero();
     x.setZero();
@@ -77,14 +77,7 @@ Eigen::MatrixXd PoissonSolver::solve(std::function<double(Point)> dirichlet, std
         int i = 0;
         int index;
         for (std::set<Point>::iterator p = domain.begin(); p != domain.end(); p++, i++) {
-            coefficients.push_back(Eigen::Triplet<double>(i, i, numNeighbors(*p)));
             std::set<Point> pNeighbors = neighbors(*p);
-            for (std::set<Point>::iterator q = pNeighbors.begin(); q != pNeighbors.end(); q++) {
-                index = domainMask.at<int>(q->y, q->x);
-                if (index != -1) {
-                    coefficients.push_back(Eigen::Triplet<double>(i, index, -1));
-                }
-            }
             for (std::set<Point>::iterator q = pNeighbors.begin(); q != pNeighbors.end(); q++) {
                 index = domainMask.at<int>(q->y, q->x);
                 if (index == -1) {
@@ -96,9 +89,6 @@ Eigen::MatrixXd PoissonSolver::solve(std::function<double(Point)> dirichlet, std
             }
         }
     }
-    A.setFromTriplets(coefficients.begin(), coefficients.end());
-    Eigen::UmfPackLU<Eigen::SparseMatrix<double>> solver;
-    solver.compute(A);
     x = solver.solve(b);
     return x;
 }
